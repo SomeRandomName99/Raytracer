a7149e3e6dacf86c7f5e11c96c3e48cebc5f13bd
SomeRandomName99 <husam.malkawi@hotmail.com>
2023-04-01 18:39:02 +0200

    Added documentation

--- src/buildingBlocks/include/sphere.h
+++ src/buildingBlocks/include/sphere.h
@@ -11,5 +11,13 @@ public:
   Sphere() = default;
   ~Sphere() = default;
 
+  /*
+  * \brief Return the t values correspondig to points at which the ray intersects the sphere's surface
+  *
+  * This function will also calculate intersection behind the origin's start point, as they will be used for calculating
+  * reflectionsn and other lighting effects later on.
+  * 
+  * \param ray a Ray object contaning basic info such as direction and starting point.
+  */
   std::vector<double> intersect(const Ray::Ray& ray) const;
 };
