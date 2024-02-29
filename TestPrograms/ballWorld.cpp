fb9c96f8a52a664cabf0766f81a5f0bd8a110087
SomeRandomName99 <husam.malkawi@hotmail.com>
2024-02-29 23:37:42 +0100

    Use the new plane class in the test project

--- TestPrograms/ballWorld.cpp
+++ TestPrograms/ballWorld.cpp
@@ -18,23 +18,8 @@ int main(){
   auto wallMaterial = material::Material(utility::Color(1, 0.9, 0.9));
   wallMaterial.specular_ = 0;
 
-  auto floor = geometry::Sphere();
-  floor.setTransform(utility::transformations::scaling(10, 0.01, 10));
+  auto floor = geometry::Plane();
   floor.material_ = wallMaterial;
-
-  auto leftWall = geometry::Sphere();
-  leftWall.setTransform(utility::transformations::translation(0, 0, 5) * 
-                             utility::transformations::rotation_y(-std::numbers::pi / 4) * 
-                             utility::transformations::rotation_x(std::numbers::pi / 2) * 
-                             utility::transformations::scaling(10, 0.01, 10));
-  leftWall.material_ = wallMaterial;
-
-  auto rightWall = geometry::Sphere();
-  rightWall.setTransform(utility::transformations::translation(0, 0, 5) * 
-                              utility::transformations::rotation_y(std::numbers::pi / 4) * 
-                              utility::transformations::rotation_x(std::numbers::pi / 2) * 
-                              utility::transformations::scaling(10, 0.01, 10));
-  rightWall.material_ = wallMaterial;
 
   auto middle = geometry::Sphere();
   middle.setTransform(utility::transformations::translation(-0.5, 1, 0.5));
@@ -44,27 +29,24 @@ int main(){
 
   auto right = geometry::Sphere();
   right.setTransform(utility::transformations::translation(1.5, 0.5, -0.5) * 
-                          utility::transformations::scaling(0.5, 0.5, 0.5));
+                     utility::transformations::scaling(0.5, 0.5, 0.5));
   right.material_.surfaceColor_ = utility::Color(0.5, 1, 0.1);
   right.material_.diffuse_ = 0.7;
   right.material_.specular_ = 0.3;
 
   auto left = geometry::Sphere();
   left.setTransform(utility::transformations::translation(-1.5, 0.33, -0.75) * 
-                         utility::transformations::scaling(0.33, 0.33, 0.33));
+                    utility::transformations::scaling(0.33, 0.33, 0.33));
   left.material_.surfaceColor_ = utility::Color(1, 0.8, 0.1);
   left.material_.diffuse_ = 0.7;
   left.material_.specular_ = 0.3;
 
   scene::World world;
   world.lights_.push_back(scene::PointLight(utility::Color(1, 1, 1), utility::Point(-10, 10, -10)));
-  world.objects_.emplace_back(floor);
-  world.objects_.emplace_back(leftWall);
-  world.objects_.emplace_back(rightWall);
   world.objects_.emplace_back(middle);
   world.objects_.emplace_back(right);
   world.objects_.emplace_back(left);
-
+  world.objects_.emplace_back(floor);
 
   auto camera = scene::Camera(1920, 1080, std::numbers::pi / 3);
   camera.transform_ = utility::transformations::view_transform(utility::Point(0, 1.5, -5),
