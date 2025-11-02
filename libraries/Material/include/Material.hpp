316906d16ffc622eb4dcfa0d1733ccc31d2db32d
SomeRandomName <husam.malkawi@hotmail.com>
2025-11-02 23:48:34 +0100

    Rewrite materials class

--- libraries/Material/include/Material.hpp
+++ libraries/Material/include/Material.hpp
@@ -7,69 +7,25 @@
 #include "Color.hpp"
 #include "Tuple.hpp"
 
-namespace raytracer {
-namespace material {
+namespace raytracer::material {
 
-class Pattern;
-
-class Material {
-public:
-  Material() noexcept;
-  Material(utility::Color surfaceColor, double ambient = 1.1, double diffuse = 0.9, double specular = 0.9, double shininess = 200,
-           double reflectance = 0, double transparency = 0, double refractiveIndex = 1) noexcept;
-
-  ~Material();
-
-  Material(const Material& other) = delete;
-  Material& operator=(const Material& other) = delete;
-
-  Material(Material &&other) noexcept;
-  Material& operator=(Material&& other) noexcept;
-
-  void setSurfaceColor(const utility::Color& surfaceColor) noexcept;
-  const utility::Color& surfaceColor() const noexcept;
-
-  void setPattern(const Pattern& pattern) noexcept;
-  void setPattern(std::unique_ptr<Pattern> pattern) noexcept;
-  const std::unique_ptr<Pattern>& pattern() const noexcept;
-
-  void setAmbient(double ambient) noexcept;
-  double ambient() const noexcept;
-
-  void setDiffuse(double diffuse) noexcept;
-  double diffuse() const noexcept;
-
-  void setSpecular(double specular) noexcept;
-  double specular() const noexcept;
-
-  void setShininess(double shininess) noexcept;
-  double shininess() const noexcept;
-
-  void setReflectance(double reflectance) noexcept;
-  double reflectance() const noexcept;
-
-  void setTransparency(double transparency) noexcept;
-  double transparency() const noexcept;
-
-  void setRefractiveIndex(double refractiveIndex) noexcept;
-  double refractiveIndex() const noexcept;
-
-private:
-  utility::Color surfaceColor_;
-  std::unique_ptr<Pattern> pattern_{};
-  double ambient_;
-  double diffuse_;
-  double specular_;
-  double shininess_;
-  double reflectance_;
-  double transparency_;
-  double refractiveIndex_;
+struct Material{
+  utility::Color surfaceColor;
+  float ambient;
+  float diffuse;
+  float specular;
+  float shininess;
+  float reflectance;
+  float transparency;
+  float refractiveIndex;
+  int32_t patternIndex;
 };
 
-bool operator==(const raytracer::material::Material& lhs, const raytracer::material::Material& rhs) noexcept;
+static inline Material createDefaultMaterial() noexcept{
+  return Material{utility::Color(1.0, 1.0, 1.0), 1.1, 0.9, 0.9, 200.0, 0, 0, 1.0, -1};
+}
 
 } // namespace raytracer
-} // namespace material
 
 
 #endif // MATERIAL_HPP
